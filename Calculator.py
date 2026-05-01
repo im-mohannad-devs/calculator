a15bbb961d36b1203b94cfc6b91dca4a4ca8044b
Mohanand Nader <mhndnadr2@gmail.com>
2026-05-01 16:33:26 +0300

    Refactor calculator to use functions for operations
    Just using functions and exceptions

--- Calculator.py
+++ Calculator.py
@@ -1,29 +1,67 @@
-result =  float(input("Enter the first Number or 'q' to stop:"))
+def add(a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mul(a,b):
+    return a * b
+def div(a,b):
+    return a / b
+def pow(a,b):
+    return a**b
+
+result = None
 
 while True:
-    operation = input("Which operation you want or press'q'to stop?\n(/ * - + ^):")
-    if operation == "q":
-        print("App is done.")
-        print(result)
+    print("========================================================================\n\t\t\tSimple Calculator\n========================================================================")
+    print("Choose an operation:\n====================\n1-Add\t2-Subtract\t3-Multiplication\t4-Division\t5-Power\n========================================================================\n= : show result    C : clear    0-To stop\n========================================================================")
+    choice = input("Enter operation:").upper()
+    
+    if choice not in ["1","2","3","4","5","=","C","0"]:
+        print("Invalid choice!")
+        continue
+    
+    if choice == "0":
+        print("Done.")
         break
-    if operation not in ["+", "-", "*", "/", "^"]:
-        print("Operation error")
+    
+    if choice == "C":
+        result = None
+        print("The result has deleted")
         continue
     
-    num = float(input("Enter the 2nd number:"))
-    
-    if operation == "/" and num ==0:
-        print("You cannot divide by zero")
+    if choice == "=":
+        if result is not None:
+            print("Current result:",result)
+        else:
+            print("There is no result right now.")
         continue
-    if operation == "+":
-        result += num
-    elif operation == "-":
-        result -= num
-    elif operation == "*":
-        result *= num
-    elif operation == "/":
-        result /= num
-    elif operation == "^":
-        result = pow(result,num)
+    try:
+        if result is None:
+            result = float(input("Enter first number:"))
         
-    print(f"Result:{result}")
+        else:
+            print("Current result:", result)
+            
+        num = float(input("Enter next number:"))
+            
+        if choice == "1":
+            result = add(result,num)
+        
+        elif choice =="2":
+            result = sub(result,num)
+    
+        elif choice =="3":
+            result = mul(result,num)
+            
+        elif choice =="4":
+            result = div(result,num)
+            
+        elif choice =="5":
+            result = pow(result,num)
+        
+        print("Updated result:",result)
+        
+    except ValueError:
+        print("You can write numbers only")
+    except ZeroDivisionError:
+        print("You can't divide by zero")
