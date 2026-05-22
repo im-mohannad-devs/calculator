c79b7c44e32e1ae5813bc473372f5d605c0a8183
Mohanand Nader <mhndnadr2@gmail.com>
2026-05-22 17:28:56 +0300

    Refactor calculator and enhance operator support
    Refactored calculator interface and added support for additional operators.

--- Calculator.py
+++ Calculator.py
@@ -11,24 +11,31 @@ def pow(a,b):
 
 result = None
 
+line = "=" * 72
+
 while True:
-    print("========================================================================\n\t\t\tSimple Calculator\n========================================================================")
-    print("Choose an operation:\n====================\n1-Add\t2-Subtract\t3-Multiplication\t4-Division\t5-Power\n========================================================================\n= : show result    C : clear    0-To stop\n========================================================================")
+    print("=" * 27,"Simple Calculator","=" * 26)
+    print("Choose an operation:")
+    print("=" * 15)
+    print("1-Add\t2-Subtract\t3-Multiplication\t4-Division\t5-Power")
+    print(line)
+    print("= : show result    C : clear    0-To stop")
+    print(line)
     choice = input("Enter operation:").upper()
-    
-    if choice not in ["1","2","3","4","5","=","C","0"]:
+
+    if choice not in ["1","2","3","4","5","=","C","0","+","-","*","/","**"]:
         print("Invalid choice!")
         continue
-    
+
     if choice == "0":
         print("Done.")
         break
-    
+
     if choice == "C":
         result = None
         print("The result has deleted")
         continue
-    
+
     if choice == "=":
         if result is not None:
             print("Current result:",result)
@@ -44,19 +51,19 @@ while True:
             
         num = float(input("Enter next number:"))
             
-        if choice == "1":
+        if choice == "1" or choice == "+":
             result = add(result,num)
         
-        elif choice =="2":
+        elif choice =="2" or choice == "-":
             result = sub(result,num)
     
-        elif choice =="3":
+        elif choice =="3" or choice == "*":
             result = mul(result,num)
             
-        elif choice =="4":
+        elif choice =="4" or choice == "/":
             result = div(result,num)
             
-        elif choice =="5":
+        elif choice =="5" or choice == "**":
             result = pow(result,num)
         
         print("Updated result:",result)
